4a2df022604952645e8a83614975c0245ca80d9b
Anishde85 <59112660+Anishde85@users.noreply.github.com>
2021-01-02 11:43:10 +0530

    Add files via upload

---
+++ users/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib import messages
+from users.forms import UserRegisterForm
+from django.contrib.auth import authenticate,login,logout
+def register(request):
+    if request.method=='POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('home')
+    else:
+        form = UserRegisterForm()
+    return render(request,"register.html",{'form':form})
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request,user)
+                messages.info(request, f"You are now logged in as {username}")
+                return redirect("home")
+            else:
+                messages.error(request, "Invalid username or password.")
+        else:
+            messages.error(request, "Invalid username or password.")
+    form = AuthenticationForm()
+    return render(request,"login.html",{"form": form})
+def logout_view(request):
+    logout(request)
+    messages.info(request, "Logged out successfully!")
+    return redirect("verify")

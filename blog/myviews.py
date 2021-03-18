8de17e3bc29f474b6b4ef17f0a83c9192f7da2eb
Anish De <59112660+Anishde85@users.noreply.github.com>
2021-03-18 18:33:42 +0530

    Add files via upload

--- blog/myviews.py
+++ blog/myviews.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from blog.models import MyModelName
+from blog.forms import GeeksForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 @login_required
 def hello_geek1(request):
-    userw = request.user
-    post = [i for i in MyModelName.objects.filter(author=userw)]
-    return render(request, "index1.html", {'posts': post})
+    inp = GeeksForm(request.POST or None)
+    if request.method == 'POST' and inp.is_valid():
+        title1 = inp.cleaned_data['title']
+        content1=inp.cleaned_data['description']
+        er=MyModelName.objects.all()
+        userw=request.user
+        record1=MyModelName(title=title1, content=content1,author=userw)
+        record1.save()
+        post = [i for i in MyModelName.objects.filter(author=userw)]
+        return render(request, "index1.html", {'form':inp,'posts': post})
+    else:
+        userw = request.user
+        post = [i for i in MyModelName.objects.filter(author=userw)]
+        return render(request, "index1.html", {'form':inp,'posts': post})
